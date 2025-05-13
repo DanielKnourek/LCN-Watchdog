f22efdce575bd11adcb0d199b0414396a95d0d04
Daniel Kňourek <dev.danielknourek@gmail.com>
2025-05-13 22:23:33 +0000

    feat: evaluateScoreWebzio

--- src/server/lib/mediaDowloader.ts
+++ src/server/lib/mediaDowloader.ts
@@ -1,19 +1,35 @@
+import { publicDecrypt } from "crypto";
+import { uuid } from "drizzle-orm/gel-core";
+import { url } from "inspector";
 import { z } from "zod";
 import { env } from "~/env";
 
-// https://api.webz.io/newsApiLite?token=${env.WEBZIO_API_KEY}&q=Google topic:"financial and economic news" sentiment:negative
+const MediaWebzioSchema = z.object({
+  totalResults: z.number().min(0),
+  requestsLeft: z.number(),
+  posts: z.array(z.object({
+    uuid: z.string(),
+    // uuid: z.string().uuid(), // cannot parse, not a stnadard uuid
+    title: z.string(),
+    published: z.string(),
+    url: z.string(),
+  })),
+});
 
-const SENTIMENT_FILTER = 'negative';
-const QUERY_FILTER = 'Google';
+type MediaWebzio = z.infer<typeof MediaWebzioSchema>;
 
-interface dowloadMediaParams {
+
+interface dowloadMediaWebzioParams {
   query_name: string;
   sentiment: 'negative' | 'positive' | 'neutral';
   min_time?: Date;
   max_time?: Date;
 }
 
-const dowloadMedia = async({query_name, sentiment, min_time, max_time}:dowloadMediaParams) => {
+/**
+ * This function downloads media data from Webz.io API based on the provided parameters.
+ */
+const dowloadMediaWebzio = async({query_name, sentiment, min_time, max_time}:dowloadMediaWebzioParams) => {
   if (min_time === undefined) { // TODO: move out function
     min_time = new Date(Date.now()-1000 * 60 * 60 * 24); // default to 24 hours ago
   }
@@ -21,18 +37,17 @@ const dowloadMedia = async({query_name, sentiment, min_time, max_time}:dowloadMe
     `${query_name}`,
     'topic:"financial and economic news"',
     `sentiment:${sentiment}`,
-    'domain_rank:<1000',
+    // 'domain_rank:<1000',
     `published:>${min_time.getTime()}`,
   ];
 
   const request_uri = `https://api.webz.io/newsApiLite?token=${env.WEBZIO_API_KEY}&q=${query_filters.join(' ')}`;
 
-  console.log(">>||query_filters<<||", request_uri); //TODO remove log
   const data = await fetch(request_uri)
   .then((res) => {
     if (!res.ok) {
       //TODO log error
-      console.error("Error fetching data:", res.statusText);
+      console.error("Error fetching data:", res.statusText); //TODO remove log
       if (res.status === 500) {
         throw new Error("Failed to execute query: internal error");
       }
@@ -40,22 +55,73 @@ const dowloadMedia = async({query_name, sentiment, min_time, max_time}:dowloadMe
     }
     return res.json();
   })
+  .then((data) => {
+    return MediaWebzioSchema.parseAsync(data);
+  })
+  .catch((error) => {
+    console.error("Error parsing data:", error); //TODO remove log
+    throw new Error("Failed to parse data");
+  });
+
   // console.log("data", data); //TODO remove log
-  console.log(`querry: |${query_name}| sentiment: |${sentiment}|`); //TODO remove log
+  // console.log(`querry: |${query_name}| sentiment: |${sentiment}|`); //TODO remove log
   // TODO log how many requests remaining
   return data;
 };
 
+interface evaluateScoreWebzioParams {
+  positive: MediaWebzio;
+  negative: MediaWebzio;
+  neutral: MediaWebzio;
+}
+/**
+ * This function evaluates the score of media data from Webz.io API based on the provided parameters.
+ * It calculates the ratio of positive, negative, and neutral sentiments. and normalizes the score to range < -10,10 > integers
+ */
+const evaluateScoreWebzio = ({positive, negative, neutral}:evaluateScoreWebzioParams) => {
+  const NEUTRAL_WEIGHT = 0.1; // TODO: add to env
+  const AMPLIFY_FINAL = 1.5; // TODO: add to env
+  const neutral_score = neutral.totalResults * NEUTRAL_WEIGHT;
+  const positive_score = positive.totalResults;
+  const negative_score = negative.totalResults;
+
+  // if no data, return score 0
+  if (positive_score === 0 && negative_score === 0 && neutral_score === 0) {
+    return 0;
+  }
+  const total_score = positive_score + negative_score + neutral_score;
+  const positive_ratio = positive_score / total_score;
+  const negative_ratio = negative_score / total_score;
+
+  let score = positive_ratio - negative_ratio;
+  score = score * AMPLIFY_FINAL; // amplify the score
+  let normalized_score = Math.round(score *10); // normalize to range < -10,10 >
+  
+  
+  if (normalized_score > 10) {
+    normalized_score = 10;
+  } else if (normalized_score < -10) {
+    normalized_score = -10;
+  }
+
+  return normalized_score;
+}
 
 const run = async() => {
+  const QUERY_FILTER = 'Google';
   console.log("run media downloader"); //TODO remove log
-  console.log(env.DATABASE_URL); //TODO remove log
-  const data = await dowloadMedia({
+  const data = await dowloadMediaWebzio({
     query_name: QUERY_FILTER,
     sentiment: 'positive',
     min_time: new Date(Date.now() - 1000 * 60 * 60 * 24)
   });
 
+  const result = evaluateScoreWebzio({
+    positive: data,
+    negative: {...data, totalResults: 140},
+    neutral: {...data, totalResults: 140}
+  });
+  
   // const data2 = await dowloadMedia({
   //   query_name: QUERY_FILTER,
   //   sentiment: 'negative',
@@ -69,7 +135,8 @@ const run = async() => {
   // });
   // console.log(`positive_data_totalResults: |${data.totalResults}|\negative_data_totalResults: |${data2.totalResults}|\neutral_data_totalResults: |${data3.totalResults}|\n`); //TODO remove log
   // return { positive_data: data, negative_data: data2, neutral_data: data3 };
-  return { positive_data: data};
+  return { positive_data: result};
 };
 
+export { dowloadMediaWebzio, evaluateScoreWebzio};
 export default run;
---
+++ test/src/server/lib/mediaDowloader.test.ts
@@ -0,0 +1,53 @@
+import { describe, it, expect } from 'vitest'
+
+import { evaluateScoreWebzio } from '~/server/lib/mediaDowloader'
+
+describe('Media Downloader', () => {
+    describe('evaluateScoreWebzio', () => {
+        const sampleFigure = { totalResults: 0, requestsLeft: 100, posts: [] };
+        it('should return zero when no data', async () => {
+            const result = evaluateScoreWebzio({
+                positive: { ...sampleFigure, totalResults: 0 },
+                negative: { ...sampleFigure, totalResults: 0 },
+                neutral: { ...sampleFigure, totalResults: 0 },
+            })
+            expect(result).toEqual(0)
+        });
+        it('should return 10 when all data is positive', async () => {
+            const result = evaluateScoreWebzio({
+                positive: { ...sampleFigure, totalResults: 100 },
+                negative: { ...sampleFigure, totalResults: 0 },
+                neutral: { ...sampleFigure, totalResults: 0 },
+            })
+            expect(result).toEqual(10)
+        });
+        it('should return -10 when all data is negative', async () => {
+            const result = evaluateScoreWebzio({
+                positive: { ...sampleFigure, totalResults: 0 },
+                negative: { ...sampleFigure, totalResults: 1000 },
+                neutral: { ...sampleFigure, totalResults: 0 },
+            })
+            expect(result).toEqual(-10)
+        });
+        it('should return slightly positive when positive is greater than negative', async () => {
+            const result = evaluateScoreWebzio({
+                positive: { ...sampleFigure, totalResults: 100 },
+                negative: { ...sampleFigure, totalResults: 80 },
+                neutral: { ...sampleFigure, totalResults:  0},
+            })
+            console.log(`result: ${result}`); //TODO remove log
+            expect(result).toEqual(2)
+        }
+        );
+        it('should return slightly very negative when negative is greater than positive', async () => {
+            const result = evaluateScoreWebzio({
+                positive: { ...sampleFigure, totalResults: 100 },
+                negative: { ...sampleFigure, totalResults: 300 },
+                neutral: { ...sampleFigure, totalResults: 0 },
+            })
+            console.log(`result: ${result}`); //TODO remove log
+            expect(result).toEqual(-7)
+        }
+        );
+    })
+})
